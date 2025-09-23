72db34ebf3818b9fa943bba53184f4f678f1515e
rhinocerbus <m.rubright.dev@gmail.com>
2025-09-23 13:26:20 -0400

    fill out coordinator impls in viewmodel, hook up server connection state to messages tab state

--- app/src/main/java/com/piledrive/sample_mqtt/viewmodel/SampleViewModel.kt
+++ app/src/main/java/com/piledrive/sample_mqtt/viewmodel/SampleViewModel.kt
@@ -2,7 +2,9 @@ package com.piledrive.sample_mqtt.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.piledrive.sample_mqtt.mqtt.model.MqttConnectionStatus
 import com.piledrive.sample_mqtt.mqtt.controller.MqttController
+import com.piledrive.sample_mqtt.ui.coordinators.MessagesCoordinator
 import com.piledrive.sample_mqtt.ui.coordinators.ServerConnectCoordinator
 import com.piledrive.sample_mqtt.ui.coordinators.TabDestination
 import com.piledrive.sample_mqtt.ui.coordinators.TabDestinations
@@ -15,16 +17,33 @@ class SampleViewModel @Inject constructor(
 	private val mqtt: MqttController
 ) : ViewModel() {
 
-	val tabsCoordinator = TabsCoordinator(
-		listOf(
-			TabDestination(TabDestinations.SERVER, enabled = true),
-			TabDestination(TabDestinations.MESSAGES, enabled = true)
-		),
-		TabDestinations.SERVER
-	)
-
 	val serverConnectCoordinator = ServerConnectCoordinator(
 		coroutineScope = viewModelScope,
 		mqtt = mqtt
 	)
+
+	val messagesCoordinator = MessagesCoordinator(
+		coroutineScope = viewModelScope,
+		mqtt = mqtt
+	)
+
+	val tabsCoordinator = TabsCoordinator(
+		viewModelScope,
+		listOf(
+			TabDestination(TabDestinations.SERVER, enabled = true),
+			TabDestination(TabDestinations.MESSAGES, enabled = true)
+		),
+		0,
+		connectionStatusSourceState = serverConnectCoordinator.connectionState,
+		onConnectionStatusChanged = { status -> updateMessagesTag(status) }
+	)
+
+	private fun updateMessagesTag(status: MqttConnectionStatus) {
+		tabsCoordinator.findTab(TabDestinations.MESSAGES)?.also {
+			it.enabled = status == MqttConnectionStatus.CONNECTED
+			if(it.enabled) {
+				tabsCoordinator.changeTab(it.tab)
+			}
+		}
+	}
 }
